ededeaf3e265c2df69f70a357bbdeaf99a3174ff
pramod toraskar <pramodto@cybage.com>
2015-10-17 18:19:53 +0530

    Debug prints and deep understanding

--- arma_sunspotsdata.py
+++ arma_sunspotsdata.py
@@ -48,19 +48,25 @@ print '-'*30
 print dta.values.squeeze()
 
 fig = plt.figure(figsize=(12,12))
+
 ax1 = fig.add_subplot(211)
+print ax1
+print type(ax1)
+
+print sm.graphics.tsa.plot_acf
+print sm.graphics.tsa.plot_pacf
+
+print '--Optional--'
+
 fig = sm.graphics.tsa.plot_acf(dta.values.squeeze(), lags=40, ax=ax1)
+
 ax2 = fig.add_subplot(212)
+
 fig = sm.graphics.tsa.plot_pacf(dta, lags=40, ax=ax2)
-fig.savefig('arma_sunspotsdata.png', dpi=200)
-
-print '-'*30
-exit()
-
-#print type(fig)
-#print dir(plt)
 
 #fig.savefig('arma_sunspotsdata.png', dpi=200)
+
+print '-'*30
 
 arma_mod20 =  sm.tsa.ARMA(dta, (2,0)).fit()
 
@@ -69,6 +75,10 @@ arma_mod20 =  sm.tsa.ARMA(dta, (2,0)).fit()
 arma_mod30 = sm.tsa.ARMA(dta, (3,0)).fit()
 
 #print (arma_mod30.params)
+
+arma_mod40 = sm.tsa.ARMA(dta, (4,0)).fit()
+
+print (arma_mod40.params)
 
 #print(arma_mod20.aic, arma_mod20.bic, arma_mod20.hqic)
 #print(arma_mod30.aic, arma_mod30.bic, arma_mod30.hqic)
@@ -81,9 +91,7 @@ ax = arma_mod30.resid.plot(ax=ax)
 #fig.savefig('arma_sunspotsdata_after_durbin_watson.png',dpi=200)
 
 resid = arma_mod30.resid
-
 stats.normaltest(resid)
-
 fig = plt.figure(figsize=(12,8))
 ax = fig.add_subplot(111)
 fig = qqplot(resid, line='q', ax=ax, fit=True)
@@ -96,23 +104,19 @@ ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
 #fig.savefig('arma_sunspotsdata_autocorrelation.png', dpi=200)
 
-
 r,q,p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
 print r,q,p
 
 data = np.c_[range(1,41), r[1:],q,p]
-
 print data
 
 table = pd.DataFrame(data, columns=['lag','AC', 'Q', 'Prob(>Q)'])
-
 print(table.set_index('lag'))
 
 print '-----------------------------------------------------------------'
 
 
 predict_sunspots = arma_mod30.predict('1990', '2012', dynamic=True)
-
 print predict_sunspots
 
 
