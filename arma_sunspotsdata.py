65d5e52ea7a1609ae4c3823c36ed5c8f63c482df
pramod toraskar <pramodto@cybage.com>
2015-10-17 18:59:12 +0530

    Add naming con. for graph

--- arma_sunspotsdata.py
+++ arma_sunspotsdata.py
@@ -32,6 +32,7 @@ print '-'*30
 
 # Remove the 'YEAR' column after re-indexing
 del dta["YEAR"]
+
 # Or dta = dta['SUNACTIVITY']
 print dta
 
@@ -39,10 +40,10 @@ print '-'*30
 
 print '-- Visualization -- '
 
-#print dta.ix[::]
+print dta.ix[::]
 
 dta.plot(y='SUNACTIVITY')
-#plt.savefig('arma_sunspotsdata_befor_sm.png', dpi=200)
+plt.savefig('1_arma_sunspotsdata_befor_sm.png', dpi=200)
 
 print '-'*30
 print dta.values.squeeze()
@@ -59,28 +60,28 @@ fig = sm.graphics.tsa.plot_acf(dta.values.squeeze(), lags=40, ax=ax1)
 ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(dta, lags=40, ax=ax2)
 
-#fig.savefig('arma_sunspotsdata.png', dpi=200)
+fig.savefig('2_arma_sunspotsdata.png', dpi=200)
 print '-'*30
 
 arma_mod20 =  sm.tsa.ARMA(dta, (2,0)).fit()
-#print (arma_mod20.params)
+print (arma_mod20.params)
 
 arma_mod30 = sm.tsa.ARMA(dta, (3,0)).fit()
-#print (arma_mod30.params)
+print (arma_mod30.params)
 
 arma_mod40 = sm.tsa.ARMA(dta, (4,0)).fit()
-#print (arma_mod40.params)
+print (arma_mod40.params)
 
 print(arma_mod20.aic, arma_mod20.bic, arma_mod20.hqic)
 print(arma_mod30.aic, arma_mod30.bic, arma_mod30.hqic)
 print(arma_mod40.aic, arma_mod40.bic, arma_mod40.hqic)
 
-#print arma_mod30.resid.values
+print arma_mod30.resid.values
 
 fig = plt.figure(figsize=(12,8))
 ax = fig.add_subplot(111)
 ax = arma_mod30.resid.plot(ax=ax)
-#fig.savefig('arma_sunspotsdata_after_durbin_watson.png',dpi=200)
+fig.savefig('3_arma_sunspotsdata_after_durbin_watson.png',dpi=200)
 
 resid = arma_mod30.resid
 
@@ -96,19 +97,25 @@ print '-'*30
 fig = plt.figure(figsize=(12,8))
 ax = fig.add_subplot(111)
 fig = qqplot(resid, line='q', ax=ax, fit=True)
-#fig.savefig('arma_sunspotsdata_normaltest.png', dpi=200)
+fig.savefig('4_arma_sunspotsdata_normaltest.png', dpi=200)
 
 fig = plt.figure(figsize=(12,8))
 ax1 = fig.add_subplot(211)
 fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=40, ax=ax1)
 ax2 = fig.add_subplot(212)
 fig = sm.graphics.tsa.plot_pacf(resid, lags=40, ax=ax2)
-#fig.savefig('arma_sunspotsdata_autocorrelation.png', dpi=200)
+fig.savefig('5_arma_sunspotsdata_autocorrelation.png', dpi=200)
 
 print '-'*30
 
 r,q,p = sm.tsa.acf(resid.values.squeeze(), qstat=True)
 print r,q,p
+
+
+print range(1,41)
+print r[1:]
+print q
+print p
 
 data = np.c_[range(1,41), r[1:],q,p]
 
@@ -127,8 +134,7 @@ print predict_sunspots
 fig, ax = plt.subplots(figsize=(12, 8))
 ax = dta.ix['1950':].plot(ax=ax)
 fig = arma_mod30.plot_predict('1990', '2012', dynamic=True, ax=ax, plot_insample=False)
-#fig.savefig('arma_sunspotsdata_forecast.png', dpi=200)
+
+fig.savefig('6_arma_sunspotsdata_forecast.png', dpi=200)
 
 print mean_forecast_err(dta.SUNACTIVITY, predict_sunspots)
-
-
